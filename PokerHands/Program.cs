a25e214a7b58a0173eda39e3756c77c8cf3b6e1e
Alexander Lau <alau@nyu.edu>
2017-11-27 13:43:00 -0500

    Updated Comments on Main Program, created readme on Github page.

--- PokerHands/Program.cs
+++ PokerHands/Program.cs
@@ -9,58 +9,9 @@ using System.Threading.Tasks;
 namespace PokerHands
 {
     /// <summary>
-    /// EA Test evalation for Poker Hands. Based on standard poker, no variations. (Not hold'm, no river)
-    /// Test: https://docs.google.com/document/d/1PPYM_fCUFjFQlUX4PCCwknQdfgde8GCDH10jD0kh-5c/edit
-    /// Hands: https://en.wikipedia.org/wiki/List_of_poker_hands
-    /// 
-    /// As a design choice, this program was made for readability and modularity rather than 
-    /// optimized performance. It is possible to create highly optimized card readers with 
-    /// tricks like bit registers/etc, but since modern devices are so fast I have erred on
-    /// the side of maintainability. Additionally, for an interview exercise I believe it is 
-    /// better to show off code and design
-    /// 
-    /// Project Layout: The code for the poker hands is in the "PokerHands" project, and the test code
-    /// is in "PokerHandsTests" which references PokerHands.
-    /// 
-    /// Usage: The driver program doesn't do anything. Instead, run the NUnit tests. If you ran the solution
-    /// in Visual Studio from the git, it should automatically download NUnit. If not, add the NUnit reference 
-    /// onthe PokerHandsTests. I run the tests using the NUnit Visual Studio plugin, but you may run them 
-    /// with whatever tool you like
-    /// 
-    /// Testing: I have only implemented some unit tests for convenience. Ideally we would want
-    /// 100% code coverage but that's too much labor for an interview question. I have provided
-    /// the included tests for code-style evaluation.
-    /// 
-    /// Testing Automation: In a production environment we would want to have the ability to quickly
-    /// generate hands and do so expediantly in something like a console. We would also want an automated test
-    /// where the computer might play multiple games against itself. However, that is outside the scope of this
-    /// interview evaluation.
-    /// 
-    /// Comments: As per Clean Code by Robert C. Martin, I only add comments where necessary and let
-    /// the code be the comment. I can adapt to a different style if necessary.
-    /// 
-    /// NOTES ON GAME RULES
-    /// 
-    /// Jokers: As this is an interview evaluation, I have elected to use casino rules and to not include jokers.
-    /// Adding jokers would add more labor and require me to write a significant amount of edge and test cases for them.
-    /// However we could extend the evaluation classes to change our verification algorithms to allow jokers.
-    /// I added an example of a possible joker extension in FlushEvaluator and the Flush tests under PokerHandsTests
-    /// 
-    /// 5 of a Kind: Because I elected not to use jokers, 5 a kind is not implemented here.
-    /// 
-    /// Cheating/Multideck: I don't account for cheating or multiple decks, as that is outside the scope of the test. 
-    /// This means it's possible to hands like 4 of a kind with all cards in the same suite if you set it up that way.
-    /// There are likely strange combos due to this, I'm letting it go to account for labor.
-    /// 
-    /// Aces: Aces are not high for this program
-    /// 
-    /// Driver Program: The driver program generates two hands, lists what the hands are and then tells us 
-    /// the winning hand. Press q to quit or any key to generate hands
-    /// 
-    /// 
-    /// Author: Alex Lau
+    /// Main program that generates two random poker hands and compares them.
+    /// Does not utilize a deck so illegal hands are possible.
     /// </summary>
-    /// 
     class Program
     {
         static void Main(string[] args)
